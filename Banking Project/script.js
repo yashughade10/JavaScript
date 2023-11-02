18f905089a23ff18b5c012ab4cfed377d99e5f19
yashughade10 <yashughade24@gmail.com>
2023-11-02 17:32:41 +0530

    Banking project added

---
+++ Banking Project/script.js
@@ -0,0 +1,37 @@
+let balanceMoney = 15080.72;
+        let checkBalance = () => {
+            document.getElementById('bal').innerHTML = `Balance : &#x20B9 xxxxx.xx`
+            document.getElementById('loader').style.visibility = "visible";
+            setTimeout(() => {
+                document.getElementById('loader').style.visibility = "hidden";
+                document.getElementById('bal').innerHTML = `Balance : &#x20B9 ${balanceMoney}`
+            }, 3000);
+
+            // document.getElementById('loader').innerHTML = `Balance : &#x20B9 ${balanceMoney}`
+            // setTimeout(() => {
+            // }, 4000);
+        }
+
+        let num = 1;
+        const transferMoney = () => {
+            let amountTransfer = parseFloat(document.getElementById('transferAmount').value);
+            let amountTo = document.getElementById('transferTo').value;
+
+            balanceMoney = balanceMoney - amountTransfer;
+            document.getElementById('transferAmount').value = "";
+            document.getElementById('transferTo').value = "";
+
+            document.getElementById(`t-${num}`).innerHTML = `&#x20B9 ${amountTransfer} transfered to ${amountTo}`;
+            document.getElementById('bal').innerHTML = `Balance : &#x20B9 ${balanceMoney}`
+            num++;
+        }
+
+        const loanRequest = () => {
+            let loanValue = parseFloat(document.getElementById('loanAmount').value);
+            confirm(`You have applied for loan of Rs ${loanValue}. Are you sure!`)
+            balanceMoney = balanceMoney + loanValue;
+            document.getElementById(`t-${num}`).innerHTML = `&#x20B9 ${loanValue} credited to your account as a Loan`;
+            document.getElementById('bal').innerHTML = `Balance : &#x20B9 ${balanceMoney}`
+            document.getElementById('loanAmount').value = "";
+            num++;
+        }
